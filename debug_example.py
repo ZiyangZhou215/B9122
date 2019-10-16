168218bbcdeecf022fdd78193eb8f69aaf47955e
ZhouZiyang <zhouziyang@ZhouZiyangs-MacBook-Pro.local>
2019-10-15 23:25:27 -0400

    add debug_example.py

---
+++ debug_example.py
@@ -0,0 +1,16 @@
+def even_check(a):
+    if (a%2==0):
+        return True
+    else:
+        return False
+
+def square_number(a):
+    b = a**2
+    print(str(b))
+
+m = 2
+n = m*2
+square_number(n)
+k=3
+
+5+2=7
